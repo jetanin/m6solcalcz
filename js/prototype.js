4bcb56863504a5af6cf2ebcb14c07047d2ef7ec8
Vahalla <97624409+jetanin@users.noreply.github.com>
2023-12-31 04:20:55 +0700

    2023_12_31

--- js/prototype.js
+++ js/prototype.js
@@ -1,15 +1,22 @@
 // Responsive Nav Bar
 let icon = document.querySelector(".icon");
 let ul = document.querySelector("ul");
+let nav_state = 0;
+
+let CT = document.querySelector(".Contents");
 
 icon.addEventListener("click", ()=>{
   ul.classList.toggle("showData");
   // console.log(ul);
   if(ul.className == "showData"){
     document.getElementById("bar").className="fa-solid fa-xmark"
+    nav_state = 1;
+    CT.style.transform = `translate(${0}px, ${280}px)`; //Slide Down Content When Showing NavMenu
   }
   else{
     document.getElementById("bar").className="fa-solid fa-bars"
+    nav_state = 0;
+    CT.style.transform = `translate(${0}px, ${0}px)`;//Slide Up Content Back
   }
 })
 
@@ -25,20 +32,27 @@ function moveLogo() {
     x += speedX;
     y += speedY;
 
-    const minX = window.innerWidth;
-    const minY = window.innerHeight;
     const maxX = window.innerWidth - SFLogo.clientWidth;
-    const maxY = window.innerHeight - (SFLogo.clientHeight + 100);
+    const maxY = window.innerHeight - (SFLogo.clientHeight + 100);   
 
-    if (x >= maxX || x <= minX) {
-      speedX *= -1;
+    if (x >= maxX || x <= 0) {
+        speedX *= -1;
     }
-    if (y >= maxY || y <= minY) {
-      speedY *= -1;
+    if (y >= maxY || y <= 0) {
+        speedY *= -1;
     }
 
+    
     SFLogo.style.transform = `translate(${x}px, ${y}px)`;
     requestAnimationFrame(moveLogo);
+    // console.log(x,y);
+
+    if(nav_state == 1 && y < 280){
+      SFLogo.style.opacity = 0;
+    }
+    else{
+      SFLogo.style.opacity = 100;
+    }
 }
 
 moveLogo();
