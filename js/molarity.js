8d0637c55db5697b4c3606ee65b691c6873ab727
Vahalla <97624409+jetanin@users.noreply.github.com>
2024-01-18 16:09:13 +0700

    inline css molarity 2024_01_18

---
+++ js/molarity.js
@@ -0,0 +1,57 @@
+// Responsive Nav Bar
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+let nav_state = 0;
+
+let CT = document.querySelector(".Contents");
+
+icon.addEventListener("click", ()=>{
+  ul.classList.toggle("showData");
+  // console.log(ul);
+  if(ul.className == "showData"){
+    document.getElementById("bar").className="fa-solid fa-xmark"
+    nav_state = 1;
+    CT.style.transform = `translate(${0}px, ${280}px)`; //Slide Down Content When Showing NavMenu
+  }
+  else{
+    document.getElementById("bar").className="fa-solid fa-bars"
+    nav_state = 0;
+    CT.style.transform = `translate(${0}px, ${0}px)`;//Slide Up Content Back
+  }
+})
+
+function Cal2Mass() {
+  let Conc = document.getElementById("Conc").value;
+  console.log(Conc);
+  let MW = document.getElementById("MW").value;
+  console.log(MW);
+  let Volome = document.getElementById("Volome").value;
+  console.log(Volome);
+  let Mass = (MW*Conc*Volome)/1000;
+  console.log(Mass);
+  document.getElementById("MassVal").textContent = Mass;
+}
+
+function Cal2C() {
+  let Mass = document.getElementById("Mass").value;
+  console.log(Mass);
+  let MW = document.getElementById("MW").value;
+  console.log(MW);
+  let Volome = document.getElementById("Volome").value;
+  console.log(Volome);
+  let Conc = ((Mass*1000)/(MW*Volome))
+  console.log(Conc);
+  document.getElementById("ConcVal").textContent = Conc;
+}
+
+function Cal2V() {
+  let Mass = document.getElementById("Mass").value;
+  console.log(Mass);
+  let MW = document.getElementById("MW").value;
+  console.log(MW);
+  let Volome = document.getElementById("Volome").value;
+  console.log(Volome);
+  let Conc = ((Mass*1000)/(MW*Volome))
+  console.log(Conc);
+  document.getElementById("ConcVal").textContent = Conc;
+}
