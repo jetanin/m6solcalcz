5c2ea11fb84bd43ae0ffe3ec1a7a6e2fdc46098a
Vahalla <97624409+jetanin@users.noreply.github.com>
2024-01-19 02:18:03 +0700

    calculate M&m 2024_01_19

--- js/molality.js
+++ js/molality.js
@@ -24,16 +24,16 @@ icon.addEventListener("click", ()=>{
 // ================== CALCULATE ============================================
 function Cal2C() {
   let Conc = -1;
-  let Volume = document.getElementById("Volume").value;
   let Mol = document.getElementById("Mol").value;
-  let Mass = document.getElementById("Mass").value;
+  let MSl = document.getElementById("MSl").value;
+  let MSv = document.getElementById("MSv").value;
   let MW = document.getElementById("MW").value;
 
-  if(Mol != 0 && Volume != 0){
-    Conc = (Mol*1000)/Volume;
+  if(Mol != 0 && MSv != 0){
+    Conc = Mol/MSv;
   }
-  else if(Mass != 0 && MW != 0 && Volume != 0){
-    Conc = ((Mass*1000)/(MW*Volume));
+  else if(MSl != 0 && MSv != 0 && MW != 0){
+    Conc = ((MSl*1000)/(MSv*MW));
   }
   else{
     Conc = "N/A"
@@ -44,15 +44,15 @@ function Cal2C() {
 function Cal2Mol() {
   let Conc = document.getElementById("Conc").value;
   let Mol = -1;
-  let Volume = document.getElementById("Volume").value;
-  let Mass = document.getElementById("Mass").value;
+  let MSl = document.getElementById("MSl").value;
+  let MSv = document.getElementById("MSv").value;
   let MW = document.getElementById("MW").value;
 
-  if(Mass != 0 && MW != 0){
-    Mol = Mass/MW
+  if(Conc != 0 && MSv != 0){
+    Mol = Conc*MSv;
   }
-  else if(Conc != 0 && Volume != 0){
-    Mol = (Conc*Volume)/1000;
+  else if(MSl != 0 && MW != 0){
+    Mol = MSl/MW;
   }
   else{
     Mol = "N/A";
@@ -60,40 +60,40 @@ function Cal2Mol() {
   document.getElementById("MolVal").textContent = Mol;
 }
 
-function Cal2V() {
+function Cal2MSl() {
   let Conc = document.getElementById("Conc").value;
   let Mol = document.getElementById("Mol").value;
-  let Volume = -1;
-  let Mass = document.getElementById("Mass").value;
+  let MSl = -1;
+  let MSv = document.getElementById("MSv").value;
+  let MW = document.getElementById("MW").value;
+
+  if(Conc != 0 && MSv != 0&& MW != 0){
+    MSl = (Conc*MW*MSv)/1000;
+  }
+  else if(Mol != 0 && MW != 0){
+    MSl = Mol*MW;
+  }
+  else{
+    MSl = "N/A"
+  }
+  document.getElementById("MSlVal").textContent = MSl;
+}
+
+function Cal2MSv() {
+  let Conc = document.getElementById("Conc").value;
+  let Mol = document.getElementById("Mol").value;
+  let MSl = document.getElementById("MSl").value;
+  let MSv = -1;
   let MW = document.getElementById("MW").value;
 
   if(Conc != 0 && Mol != 0){
-    Volume = (Mol*1000)/Conc;
+    MSv = Mol/Conc;
   }
-  else if(Conc != 0 && Mass != 0 && MW != 0){
-    Volume = ((Mass*1000)/(MW*Conc));
-  }
-  else{
-    Volume = "N/A"
-  }
-  document.getElementById("VVal").textContent = Volume;
-}
-
-function Cal2Mass() {
-  let Conc = document.getElementById("Conc").value;
-  let Mol = document.getElementById("Mol").value;
-  let Volume = document.getElementById("Volume").value;
-  let Mass = -1;
-  let MW = document.getElementById("MW").value;
-
-  if(Mol != 0 && MW != 0){
-    Mass = Mol*MW;
-  }
-  else if(MW != 0 && Conc != 0 && Volume != 0){
-    Mass = (MW*Conc*Volume)/1000;
+  else if(Conc != 0 && MSl != 0 && MW != 0){
+    MSv = (MSl*1000)/(Conc*MW);
   }
   else{
-    Mass = "N/A"
+    MSv = "N/A"
   }
-  document.getElementById("MassVal").textContent = Mass;
+  document.getElementById("MSvVal").textContent = MSv;
 }
